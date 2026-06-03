8c1286d278621b35640f7718c619ae2810b16a5b
Supreethi Gajula <gajulasupreethi@gmail.com>
2026-06-03 17:52:46 +0530

    implemented 2nd square pattern in java 123 456 789...

---
+++ PatternPrint/Pattern2.java
@@ -0,0 +1,17 @@
+
+public class Pattern2 {
+    public static void main(String [] args){
+        int n=3;
+        int num = 1;
+        //int ch = 'A';
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                //System.out.println(ch+" ");
+                System.out.print(num+" ");
+                num++;
+                //ch+=1;
+            }
+            System.out.println();
+        }
+    }
+}
