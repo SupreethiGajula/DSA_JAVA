947764fc95b247264c07c818e5811c3b92342a2a
Supreethi Gajula <gajulasupreethi@gmail.com>
2026-06-03 18:00:59 +0530

    added another variant to pattern3

--- PatternPrint/Pattern3.java
+++ PatternPrint/Pattern3.java
@@ -10,9 +10,27 @@ public class Pattern3 {
     */
     public static void main(String [] args){
         int n = 4;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i+1;j++){
-                System.out.print("*"+" ");
+        // for(int i=0;i<n;i++){
+        //     for(int j=1;j<i+1;j++){
+        //         System.out.print("*"+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        
+        /*
+        
+        1 
+        2 2 
+        3 3 3 
+        
+        
+        */
+
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<i+1;j++){
+                System.out.print(i+" ");
             }
             System.out.println();
         }
