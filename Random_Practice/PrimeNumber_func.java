9aa58c00182a0eafa86009e30c8cdb147a0389da
Supreethi Gajula <gajulasupreethi@gmail.com>
2026-08-13 16:41:59 +0530

    Boolean function to check if a number is prime

---
+++ Random_Practice/PrimeNumber_func.java
@@ -0,0 +1,17 @@
+package Random_Practice;
+
+public class PrimeNumber_func {
+    public static void isPrimeNumber(int n){
+        boolean isPrime= true;
+    for(int i=2;i<n;i++){
+        if(n%i == 0){
+            isPrime = false;
+        }
+    }
+    if(isPrime)  System.out.println(true);
+    else System.out.println(false);
+}
+public static void main(String[] args) {
+    isPrimeNumber(13);
+}
+}
